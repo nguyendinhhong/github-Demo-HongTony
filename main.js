377cea4d6ab63026e16c9ab9b76bca5b5f114186
Dinh Hong <nguyendinhhong2002@gmail.com>
2024-09-12 23:39:03 +0700

    update all

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('Xin chào các bạn')
+
+console.log(1 + 2)
+prompt('xác nhận nhập học')
+setTimeout(function(){
+    alert("hello ae")
+}, 3000)
+
+setInterval(function(){
+    console.log("tổng của 3 + 4 = " + 7)
+})
